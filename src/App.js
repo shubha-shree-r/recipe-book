4ef1bf11c2a6b5e20649b4ab526a92bf5d4cde94
Shubha Shree R <shubhashree1805@gmail.com>
2024-12-17 17:25:56 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import {books} from './book'
+import { books } from "./book";
 import Book from "./Books";
 
-
 function myApp() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
-   {books.map((book) => {
-      return  <Book book={book}/>
-   })}
-    </>
-     
-  )
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
+// function myApp() {
+//   const [state, setState] = UseState("Shubha")
+//   function handleClick() {
+//     if (state === "Shubha") {
+//       setState("Jeevan");
+//     } else {
+//       setState("Shubha");
+//     }
+//   }
+//   return (
+//     <>
+//       <h2>{state}</h2>
+//       <button type="button" onClick={handleClick}></button>
+//       {books.map((book) => {
+//         return <Book book={book} />;
+//       })}
+
+    
+//     </>
+//   );
+
 
 export default myApp;
