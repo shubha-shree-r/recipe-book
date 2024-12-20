8e06877d4270a2d3a3830bd74011c62f0d3f1089
Shubha Shree R <shubhashree1805@gmail.com>
2024-12-20 15:04:59 +0530

    react-icon

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import "../css/sidenav.css"
+import React, { useState } from "react";
+import "../css/sidenav.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope,faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 
 function SideNav() {
+  const [isOpen, setOpen] = useState(false);
 
+  function toggleNav() {
+    if (isOpen === false) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }
 
-    return (
-        <div className='sidebar'>
-            <button className="sidebar-toggle" >
-                Menu
-            </button>
-            <nav className="sidebar-nav">
-                <ul>
-                    <li>
-                        Home
-                    </li>
-                    <li>
-                        About
-                    </li>
-                    <li>
-                        Contact
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
+  return (
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
+      <button className="sidebar-toggle" onClick={toggleNav}>
+       <span> Menu</span>
+       <span> <FontAwesomeIcon icon={faArrowRight} /></span>
+       
+      </button>
+     
+      <div>
+        {/* <FontAwesomeIcon icon="fa-solid fa-user" /> */}
+        {/* <FontAwesomeIcon icon={faEnvelope} /> */}
+       
+      </div>
+      <nav className="sidebar-nav">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default SideNav;
